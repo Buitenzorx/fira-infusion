430f4a858f61ceffbd3cbcb65c3fd5a9bfea7330
Bayu Sutra <202111224bayus@gmail.com>
2024-07-15 14:12:44 +0700

    Troubleshoot

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    $weights = \App\Models\Weight::latest()->take(10)->get();
-    return view('dashboard', compact('weights'));
-});
+// Route::get('/dashboard', function () {
+//     $weights = \App\Models\Weight::latest()->take(10)->get();
+//     return view('dashboard', compact('weights'));
+// });
 
